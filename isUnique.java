a2362d72d923938fff18a3363bf4fb0379f6da14
bandicoutts <14368864+bandicoutts@users.noreply.github.com>
2019-01-12 17:29:38 +1300

    Add files via upload

---
+++ isUnique.java
@@ -0,0 +1,51 @@
+/*
+Question from "Crading the Coding Interview":
+
+Is Unique: Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
+
+This version was a minimum viable product to get something working. The next step will be to do this without needing
+any additional data structures.
+
+Hints from the book:
+Use a hash table. Vectors could be useful. Aim for N log n time.
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class isUnique {
+
+    public static String getInput(){
+        String result;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        result = reader.nextLine();
+        return result;
+    }
+
+    public static String printResult(boolean result){
+        if (result) {
+            return "The characters in this string are unique.";
+        } else {
+            return "The characters in this string are not unique.";
+        }
+    }
+
+    public static boolean checkString(String str){
+        char[] charArray = str.toCharArray();
+        Arrays.sort(charArray);
+        for (int i = 0; i < charArray.length - 1; i++){
+            if (charArray[i] == charArray[i+1]) {
+                    return false;
+            }
+        } return true;
+        }
+
+    public static void main(String[] args){
+        String stringToCheck = getInput();
+        System.out.println(stringToCheck);
+        boolean result = checkString(stringToCheck);
+        System.out.println(printResult(result));
+    }
+}
